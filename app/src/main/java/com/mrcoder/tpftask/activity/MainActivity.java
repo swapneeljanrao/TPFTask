d1c007bda57fd4ac540ed0abb86772be94ab7acf
SwapneelJanrao <swapneel.j@ideastarz.com>
2020-10-07 20:08:09 +0530

    Initial commit with no code

---
+++ app/src/main/java/com/mrcoder/tpftask/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mrcoder.tpftask.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mrcoder.tpftask.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
