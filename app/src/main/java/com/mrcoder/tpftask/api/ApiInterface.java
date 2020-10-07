1aa3fbeb9e275d5bacd0aed4b2e5c8a6406f2cb8
SwapneelJanrao <swapneel.j@ideastarz.com>
2020-10-07 23:13:46 +0530

    Api Configuration Added Classes APIClient.java ApiInterface.java

--- app/src/main/java/com/mrcoder/tpftask/api/ApiInterface.java
+++ app/src/main/java/com/mrcoder/tpftask/api/ApiInterface.java
@@ -1,19 +1,21 @@
 package com.mrcoder.tpftask.api;
 
 
+import com.mrcoder.tpftask.model.Headlines;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
     @GET("top-headlines")
-    Call<com.mrcoder.tpftask.activity.model.Headlines> getHeadlines(
+    Call<Headlines> getHeadlines(
             @Query("country") String country,
             @Query("apiKey") String apiKey
     );
 
     @GET("everything")
-    Call<com.mrcoder.tpftask.activity.model.Headlines> getSpecificData(
+    Call<Headlines> getSpecificData(
             @Query("q") String query,
             @Query("apiKey") String apiKey
     );
